6d6236db19fb84ec1669ae0b5acba28ed123f076
rakesh-csc <you@example.com>
2024-08-23 23:39:29 +0530

    first commit

---
+++ Filename.java
@@ -0,0 +1,8 @@
+                                                                               PROGRAM BOOK 
+                                                                                  FOR 
+                                                                        COMMUNITY SERVICE PROJECT
+NAME(S) OF THE STUDENT:vamsi
+
+
+NAME OF THE COLLEGE: GODAVARI INSTITUTE OF ENGINEERING AND 
+ TECHNOLOGY (AUTONOMOUS)
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+public static void main(String args[]){
+for(int i=o;i<args.length;i++)
+System.out.println(args[i]);
+}
+}
